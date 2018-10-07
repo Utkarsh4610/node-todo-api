ee7c1e562e791ae418f386bcbcf2b8e245dea4ee
Utkarsh Kumar <utkarsh.kumar4610@gmail.com>
2018-10-07 12:00:44 +0530

    'deletion_part_done'

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,26 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err)
+  {
+    return console.log('Unable to connect to MongoDb Server');
+  }
+  console.log('Connected to MongoDB Server');
+
+  //delete many
+  // db.collection('Todos').deleteMany({text: 'dup'}).then((result)=>{
+  //   console.log(result);
+  // });
+
+  //delete one
+  // db.collection('Todos').deleteOne({text: 'dul'}).then((result)=>{
+  //   console.log(result);
+  // });
+  //find one and delete
+  db.collection('Todos').findOneAndDelete({completed: false}).then((result)=>{
+    console.log(result);
+  });
+  db.close();
+
+});
